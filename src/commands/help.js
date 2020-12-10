2d953ee83330905de3428e19d80d9a3f2b66d771
GHOST <ghostdevbusiness@gmail.com>
2020-12-10 21:22:50 +0000

    feat: updated help command to not show description on all commands

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,10 +4,10 @@ module.exports = ({ commands }) => {
         /* design inspiration from https://npmjs.org/commander */
         console.log([
             'Global Options:',
-            '   -h, --help\t\t\tget help on a specific command',
+            '   -h, --help',
             '',
             'Commands:',
-            ...(commands.map(x => `   ${x.usage}${x.description ? `\t\t\t\t${x.description}` : ''}`)),
+            ...(commands.map(x => `   ${x.usage}`)),
             ''
         ].join('\n'));
     } else {
