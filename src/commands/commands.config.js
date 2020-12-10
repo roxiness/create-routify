756fad688ecb0a813c78a83d4972247aeae43f07
GHOST <ghostdevbusiness@gmail.com>
2020-12-10 21:21:21 +0000

    feat: added a commands config file

---
+++ src/commands/commands.config.js
@@ -0,0 +1,13 @@
+module.exports = [
+    {
+        name: 'create',
+        usage: 'create [options] folder',
+        path: 'create.js'
+    },
+    {
+        name: 'help',
+        usage: 'help',
+        path: 'help.js',
+        description: 'get this menu'
+    }
+];
