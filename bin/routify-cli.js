c03284efab5cd3a49f65630a9b4cec5f5d61e935
GHOST <ghostdevbusiness@gmail.com>
2020-12-26 22:00:07 +0000

    refactor: inital command

--- bin/routify-cli.js
+++ bin/routify-cli.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 const pkg = require('../package.json');
 const dash = require('dashargs');
-const path = require('path');
+const { join: joinPath } = require('path');
 
-const commands = require('../src/commands/commands.config.js');
-const args = dash.parse(process.argv.slice(2).join(' '));
+const args = dash.argv();
+const input = dash.strip(process.argv.slice(2).join(' '));
 
-const cmd = process.argv[process.argv.length - 1];
-const action = commands.filter((x) => x.name == cmd)[0];
+const command = input.split(' ')[0];
 
-if (!action) {
-    console.log('No valid command given, showing the help menu:\n');
-    require('../src/commands/help.js')({ commands });
-} else {
-    if (args.help === true || args.h === true)
-        return require('../src/commands/help.js')({ command: action });
-    require(path.join(__dirname, '../src/commands', action.path))({
-        commands,
-        args
-    });
+try {
+    const { props, run } = require(joinPath('../src/commands', command));
+
+    if (args.has('help', 'h'))
+        return require('../src/commands/help.js')({
+            command,
+            props
+        });
+
+    run({ args, input });
+} catch {
+    console.log('Error: Unable to find that subcommand');
 }
 
 // Check for updates and notify if found
