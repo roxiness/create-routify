1eaa17a77b5ebeab88806a9b379a411b6929dc70
GHOST <ghostdevbusiness@gmail.com>
2020-12-27 17:25:35 +0000

    refactor: & improvements

--- commands/create.js
+++ commands/create.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-    console.log('Not Implimented');
+module.exports = {
+    props: {
+        usage: 'create',
+        description: 'create a routify project from one of our templates',
+        args: [
+            {
+                description: 'test',
+                items: ['-t', '--test']
+            }
+        ]
+    },
+
+    run: () => {
+        console.log('Not Implimented');
+    }
 };
