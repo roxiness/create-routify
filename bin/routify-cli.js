ceeedb32e9a646a62f3f6df75bfca986b76446ba
GHOSt <ghostdevbusiness@gmail.com>
2021-11-19 21:12:15 +0000

    feat: run

--- bin/routify-cli.js
+++ bin/routify-cli.js
@@ -1,31 +1,8 @@
 #!/usr/bin/env node
-const pkg = require('../package.json');
-const dash = require('dashargs');
-const { join: joinPath } = require('path');
+import updateNotifier from 'update-notifier';
+import { run } from '../src/index.js';
+import pkg from '../package.json';
 
-const args = dash.argv();
-const input = dash.strip(process.argv.slice(2).join(' '));
+updateNotifier({ pkg }).notify();
 
-const command = input.split(' ')[0];
-
-try {
-    if (!command && args.has('help', 'h'))
-        return require('../commands/help.js').run({ args });
-
-    const { props, run } = require(joinPath('../commands', command + '.js'));
-
-    if (args.has('help', 'h'))
-        require('../commands/help.js').run({
-            command,
-            props,
-            args
-        });
-
-    run({ args, input });
-} catch (e) {
-    if (args.debug) console.error(e);
-    console.log('Error: Unable to find that subcommand');
-}
-
-// Check for updates and notify if found
-require('update-notifier')({ pkg }).notify();
+run();
