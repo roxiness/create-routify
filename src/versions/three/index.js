78ac5a86e91e5faa7658371f441717d26c645e7f
GHOSt <ghostdevbusiness@gmail.com>
2021-12-12 22:05:48 +0000

    feat: v3 cli

--- src/versions/three/index.js
+++ src/versions/three/index.js
@@ -1,4 +1,11 @@
+import { onCancel } from '../../utils/prompts.js';
+import { readdir, cp } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import prompts from 'prompts';
 import k from 'kleur';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 function text() {
     console.log();
@@ -20,8 +27,55 @@ function text() {
             'https://discord.com/invite/ntKJD5B',
         )}`,
     );
+    console.log();
 }
 
-export const run = () => {
+async function getExampleDir() {
+    const projects = await readdir(join(__dirname, './examples'));
+
+    const { project } = await prompts(
+        {
+            message: 'Please select a example project',
+            name: 'project',
+            type: 'select',
+            choices: projects.map((value) => ({ title: value, value })),
+        },
+        { onCancel },
+    );
+
+    return `./${project}`;
+}
+
+export const run = async ({ projectDir }) => {
     text();
+
+    const { projectType } = await prompts(
+        {
+            type: 'select',
+            name: 'projectType',
+            message: 'What template would you like?',
+            choices: [
+                {
+                    title: 'Skeleton Project',
+                    value: 'skeleton',
+                },
+                {
+                    title: 'Example Project',
+                    value: 'example',
+                },
+            ],
+        },
+        { onCancel },
+    );
+
+    if (!['skeleton', 'example'].includes(projectType))
+        return console.log(`  ${k.red('Unable to find type ' + projectType)}`);
+
+    const exampleDir = join(
+        __dirname,
+        projectType == 'skeleton' ? './skeleton' : await getExampleDir(),
+        '/',
+    );
+
+    await cp(exampleDir, projectDir, { recursive: true });
 };
